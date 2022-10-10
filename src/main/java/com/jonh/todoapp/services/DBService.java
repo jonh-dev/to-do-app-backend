4eba5ac42d1abf794168d87e630b0b4881bb0a71
João Carlos Schwab Zanardi <jonh.dev.br@gmail.com>
2022-10-10 11:21:16 -0300

    update: Adding more default tasks

--- src/main/java/com/jonh/todoapp/services/DBService.java
+++ src/main/java/com/jonh/todoapp/services/DBService.java
@@ -20,12 +20,15 @@ public class DBService {
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
         try {
-            Todo t1 = new Todo(null, "Study", "Study Spring with Angular", sdf.parse("25/03/2022"), false);
-            Todo t2 = new Todo(null, "Reading", "Reading book about Java", sdf.parse("10/01/2022"), true);
-            Todo t3 = new Todo(null, "Exercises", "Practice exercises for better health", sdf.parse("02/02/2022"), true);
-            Todo t4 = new Todo(null, "Pay bill", "Pay the electricity bill", sdf.parse("21/03/2022"), false);
+            Todo t1 = new Todo(null, "Study", "Study Spring with Angular", sdf.parse("25/11/2022"), false);
+            Todo t2 = new Todo(null, "Reading", "Reading book about Java", sdf.parse("10/11/2022"), false);
+            Todo t3 = new Todo(null, "Exercises", "Practice exercises for better health", sdf.parse("02/08/2022"), true);
+            Todo t4 = new Todo(null, "Pay bill", "Pay the electricity bill", sdf.parse("21/12/2022"), false);
+            Todo t5 = new Todo(null, "Reading", "Reading book about Microservices", sdf.parse("10/11/2022"), false);
+            Todo t6 = new Todo(null, "Exercises", "Practice exercises for better health", sdf.parse("03/09/2022"), true);
+            Todo t7 = new Todo(null, "Pay bill", "Pay the water bill", sdf.parse("2/08/2022"), false);
 
-            todoRepository.saveAll(Arrays.asList(t1, t2, t3, t4));
+            todoRepository.saveAll(Arrays.asList(t1, t2, t3, t4, t5, t6, t7));
         } catch (ParseException e) {
             e.printStackTrace();
         }
